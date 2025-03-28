b823db5a308841ba80379a8ab45960e9043237b9
Christofer Hansson <christofer_hansson@hotmail.com>
2025-03-28 01:01:27 +0100

    Mer funktioner med karta och sökning plus lite mer texter och styling

--- src/js/map.js
+++ src/js/map.js
@@ -2,19 +2,33 @@
 
 
 // Startposition: Göteborg koordinater
-let lat = 57.7089;
-let lon = 11.9746;
+let startlat = 57.7089;
+let startlon = 11.9746;
+let data = [];
+let marker = [];
 
 
 // Initiera kartan med Leaflet
 let map = L.map('map');
 
 
-document.querySelector("#where").addEventListener("input", filterCourses);
+
+//Hämtar det inmatade värdet i sökfältet och lägger i en variabel (gör om allt till små bokstäver så inget blir case-sensetive)
+
+const searchField = document.querySelector("#search");
+const locationEl = document.querySelector("#location");
+const latitudEl = document.querySelector("#latitud");
+const longitudEl = document.querySelector("#longitud");
+
+searchField.addEventListener("submit", encodeSearch);
+
+
+
+
 
 //Visar Gögeborg på kartan vid sidinladdning
 window.onload = () => {
-    map.setView([lat, lon], 12);
+    map.setView([startlat, startlon], 12);
 
     // Lägg till OpenStreetMap-lager
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -22,24 +36,74 @@ window.onload = () => {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+    marker = L.marker([startlat, startlon]).addTo(map);
+
 }
 
 
 
-let marker = L.marker([lat, lon]).addTo(map);
 
+function encodeSearch(event) {
+    event.preventDefault();
 
-function filterCourses() {
+    const searchInput = document.querySelector("#where").value.toLowerCase();
 
+    const encodedInput = encodeURIComponent(searchInput);
+
+    console.log(`Direkt från sökfält: ${searchInput}`);
+    console.log(`Encoded: ${encodedInput}`);
+
+    getPlace(encodedInput);
 }
 
     
 
+async function getPlace(encodedInput) {
+    try {
+        const resp = await fetch(
+            `https://nominatim.openstreetmap.org/search?addressdetails=1&q=${encodedInput}&format=jsonv2&limit=1`
+        );
+
+        const data = await resp.json();
+
+        console.log(data);
+
+     
+
+        showOnMap(data);
+
+    } catch (error) {
+        console.error("Det har uppstått ett fel: ", error);
+    }
+   
+}
 
 
+function showOnMap(data) {
+    console.log(`Place data: `);
+    console.log(data);
+
+    let {lat, lon} = data[0];
+
+    console.log(lat, lon);
+
+    let {display_name} = data [0];
+
+    console.log(display_name);
+
+
+    map.setView([lat, lon], 13);
+    marker = L.marker([lat, lon]).addTo(map);
+
+    locationEl.innerHTML = `<strong>Sökträff</strong>: ${display_name}`;
+    latitudEl.innerHTML = `<strong>Latitud</strong>: ${lat}, `;
+    longitudEl.innerHTML = `<strong>Longitud</strong>: ${lon}`;
+    
+}
+    
 
     let popupSave = L.popup()
-    .setLatLng([lat, lon])
+    .setLatLng([startlat, startlon])
     .setContent("Vill du spara den här sökningen?")
     .openOn(map);
 
